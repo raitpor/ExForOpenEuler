30cb9eff0bc23fcb04b1e01facd250acf7689b09
raitpor <928890461@qq.com>
2021-05-29 15:13:51 +0800

    重写实验5代码

---
+++ ex5_new/entity/PTE.java
@@ -0,0 +1,18 @@
+package entity;
+
+/**
+ * @author Ayase
+ * @date 2021/5/27-14:52
+ */
+public class PTE {
+    private int pageId;
+    private int pageFrameId;
+
+    public int getPageId() {
+        return pageId;
+    }
+
+    public int getPageFrameId() {
+        return pageFrameId;
+    }
+}
